449fbeaeb6d826bed63e9d1bae93236453b83e29
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-07-09 14:30:55 -0300

    feat: add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,17 @@
+import { ComponentProps } from 'react';
+
+import { twMerge } from 'tailwind-merge';
+
+type ButtonProps = ComponentProps<'button'>;
+
+export function Button({ className, ...props }: ButtonProps) {
+  return (
+    <button
+      className={twMerge(
+        'flex items-center gap-2 bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium hover:bg-lime-200',
+        className
+      )}
+      {...props}
+    />
+  );
+}
